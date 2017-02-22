6c5270f97eb1742fe4dd528f7227fccc97d6ab55
Abhay Mittal <mittalabhay94@gmail.com>
2017-02-21 20:20:31 -0500

    Sudoku => Add check_valid, getConstraints

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,11 @@
-class sudoku:
+class Sudoku:
     def __init__(self):
         self.neighbours = dict()
+        self.addNeighbours()
+        self.constraints=[]
+        self.getConstraints()
 
-    def addNeighbours(self, file):
+    def addNeighbours(self):
         for i in range(9):
             for j in range(9):
                 self.neighbours[str(i) + str(j)] = self.getNeighbours(i,j)
@@ -24,6 +27,21 @@ class sudoku:
         
         return g_x_neighbours + g_y_neighbours + box_n
 
+    def getConstraints(self):
+        for x in range(9):
+            self.constraints.append([str(x) + str(g_y) for x in range(0,9)])
+        for y in range(9):
+            self.constraints.append([str(g_x) + str(y) for y in range(0,9)])
+        for x in (0,3,6):
+            for y in range(0,3,6):
+                box_n = []
+                x_base = x
+                y_base = y
+                for i in range(3):
+                    for j in range(3):
+                        box_n.append(str(x_base + i) + str(y_base + j))
+        
+
     def consistent(self,var,assignment):
         '''
         Function to check if the current assignment to variable var is consistent
@@ -39,3 +57,11 @@ class sudoku:
     
     def check_valid(self,assignment):
         for constraint in self.constraints:
+            used_vals=dict()
+            for var in constraint:
+                if assignment[var]='-' || len(assignment[var]>1):
+                    return False
+                if  used_vals[assignment[var]] is True:
+                    return False
+                used_vals[assignment[var]]=False
+                
